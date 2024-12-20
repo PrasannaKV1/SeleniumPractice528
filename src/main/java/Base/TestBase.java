b782cf245b29dbdad85185c80bae3b600f455882
PrasannaKV1 <prasannapooja215@gmail.com>
2024-12-20 13:14:53 +0530

    this is the commit message

--- src/main/java/Base/TestBase.java
+++ src/main/java/Base/TestBase.java
@@ -1,6 +1,8 @@
 package Base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,8 +14,7 @@ public class TestBase {
     public static WebDriver driver;
     Scanner sc = new Scanner(System.in);
     String browsername =sc.nextLine();
-    public static void intialization()
-    {
+    public void intialization() throws InterruptedException {
         if(browsername.equalsIgnoreCase("chrome"))
         {
             driver=new ChromeDriver();
@@ -24,6 +25,24 @@ public class TestBase {
 
            driver=new EdgeDriver();
         }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        //driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.get("https://phase7.nprd.topschool.co.in");
+        driver.findElement(By.xpath("//*[text()='Teacher']")).click();
+        WebElement base2 =driver.findElement(By.xpath("//input[@name='userName']"));
+        base2.click();
+        Thread.sleep(2000);
+        base2.sendKeys("rrh");
+        WebElement base3=driver.findElement(By.xpath("//input[@name='password']"));
+        Thread.sleep(2000);
+        base3.sendKeys("Test@123");
+        driver.findElement(By.xpath("//*[text()='Log In']")).click();
+
+        driver.findElement(By.xpath("//*[text()='Assessment']"));
+
     }
+
+
 
 }
